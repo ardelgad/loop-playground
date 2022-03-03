3b9947de78e515ca7eff20c895939dbe381872f0
ardelgad <95305208+ardelgad@users.noreply.github.com>
2022-03-03 08:14:06 +0100

    Update ex0.js

--- ex0.js
+++ ex0.js
@@ -5,10 +5,37 @@
  * 
  * Por ejemplo, si mes = 4, deberíamos mostrar por el terminal "Nos encontramos en el segundo trimestre"  
  * 
- * Explra esta información para averiguar como usar el condicional 'switch'; y cómo se pueden agrupar varios casos: https://javascript.info/switch#grouping-of-case
+ * Explora esta información para averiguar como usar el condicional 'switch'; y cómo se pueden agrupar varios casos: https://javascript.info/switch#grouping-of-case
  * 
  */
+let mes = 10
 
-let mes = 4;
+switch (mes) {
+    case 1:
+    case 2:
+    case 3:
+        console.log("Estamos en el primer trimestre");
+        break;
+    case 4:
+    case 5:
+    case 6:
+        console.log("Estamos en el segundo trimestre");
+        break;
+    case 7:
+    case 8:
+    case 9:
+        console.log("Estamos en el tercer trimestre");
+        break;
+    case 10:
+    case 11:
+    case 12:
+        console.log("Estamos en el cuarto trimestre");
+        break;
+    default:
+    console.log("Ese mes no existe");
+        break;
+}
 
 // Escribir a partir de aquí
+
+
